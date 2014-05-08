8a725f1dc576d5b6a6ed9714c409cb36f1f54c81
Antti Stenvall <antti@stenvall.fi>
2014-05-08 10:00:57 +0300

    updated init

---
+++ dpInit.m
@@ -0,0 +1,36 @@
+%% init.m
+%
+% set modules of dp solver to Matlab path
+%
+% Created: Antti Stenvall (antti@stenvall.fi)
+
+function varargout = init()
+
+now = strrep(mfilename('fullpath'),mfilename,'');
+
+if nargout
+    varargout{1} = now;
+    return
+end
+
+%% Add modules to Matlab path
+modules = {'',... % for adding the objects and this file
+    'general',...
+    };
+
+% Set Modules to path
+newPath = path;
+
+for k=1:length(modules)
+    newDir = [now modules{k}];
+    if isempty(strfind(newPath,newDir)) % if does not yet exist
+        newPath = [newPath ';' newDir];
+    end
+end
+newPath = [newPath ';'];
+
+% Set path
+path(newPath);
+
+disp('Matlab path data changed successfully.')
+disp('Initialization finished.')
