414d2cdbe6b8224f8d4a3357ad6565b9e03f5a76
Antti Stenvall <antti@stenvall.fi>
2014-05-16 08:22:23 +0300

    Modified lab1

---
+++ @dpMesh/displayStatistics.m
@@ -0,0 +1,32 @@
+%% displayStatistics.m
+%
+% show all element and node data
+%
+% Created: Antti Stenvall (antti@stenvall.fi)
+
+function displayStatistics(this,varargin)
+defaults.basic = 0;
+param = setDefaultParameters(defaults,varargin);
+
+str = '---------------------';
+
+disp(str);
+disp('Statistics');
+disp(str);
+c = this.getCoordinates();
+disp(['Dimension: ' num2str(this.dim)]);
+disp(['Nodes: ' num2str(size(c,1))]);
+disp(str);
+disp('Elements');
+f = fields(this.msh);
+for k=1:length(f)
+    tags = this.getElementTags(f{k});
+    disp([f{k} ': ' num2str(length(tags))]);
+   if ~param.basic         
+       uTags = unique(tags);       
+       for p=uTags'           
+          disp([num2str(p) ': ' num2str(sum(p == tags))]);
+       end       
+   end    
+end
+disp(str);
